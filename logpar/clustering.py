c557a023c56fbd119d8f832d988fe8f93353a3c2
gagdiez <gagdiez.c@gmail.com>
2017-02-13 19:14:00 +0100

    changed doc-style in logpar.clustering

--- logpar/clustering.py
+++ logpar/clustering.py
@@ -25,17 +25,28 @@ def clustering(features, method='ward', constraints=None, min_size=0,
     ''' Clusters features using the selected method. If indicated, it
         constraints the clustering between neighbors UNTIL clusters reach a
         minimum size. After, the clustering continues WITHOUT constraints.
-
-        @param features: Matrix where each row is a vector of features
-        @param method: Clustering method to use (ward, centroid)
-        @param constraints: Binary matrix of #features x #features, each entry
-                            represents if two features are neighbors
-        @param min_size: Minimum size for the resulting clusters
-        @param copy: If FALSE then the features are clustered IN PLACE,
-                     modifying the INPUT features matrix.
-        @param verbose: if TRUE displays debbuging information
-
-        @return Returns a dendrogram represeting the clustering of features
+        
+        Parameters
+        ----------
+        features: array_like
+            Matrix where each row is a vector of features
+        method: string
+            Clustering method to use (ward, centroid)
+        constraints: array_like
+            Binary matrix of #features x #features, each entry
+            represents if two features are neighbors
+        min_size: int
+            Minimum amount of features in each resulting cluster
+        copy: bool
+            If FALSE then the features are clustered IN PLACE,
+            modifying the INPUT features matrix.
+        verbose: bool
+            if TRUE displays debbuging information
+        
+        Returns
+        -------
+        dendrogram
+            Returns a dendrogram represeting the clustering of features
         '''
     check_input(method, constraints, min_size)
 
